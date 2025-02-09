9c70cb54c61c44dbb03da3b844ffa13e016a7c3d
Ebrar Tikit <20200808066@ogr.akdeniz.edu.tr>
2025-02-09 14:02:51 +0300

    feat(auth): create auth page

--- app/src/main/java/com/example/smartparkingsystem/ui/auth/AuthFragment.kt
+++ app/src/main/java/com/example/smartparkingsystem/ui/auth/AuthFragment.kt
@@ -1,25 +1,53 @@
 package com.example.smartparkingsystem.ui.auth
 
+import android.graphics.Color
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.style.ForegroundColorSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.example.smartparkingsystem.R
+import com.example.smartparkingsystem.databinding.FragmentAuthBinding
 
-class AuthFragment : Fragment() {
+class AuthFragment : Fragment(R.layout.fragment_auth) {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
+    private var _binding: FragmentAuthBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_auth, container, false)
+    ): View {
+        _binding = FragmentAuthBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val welcomeText = "Welcome to GreenSpot!"
+        val spannableString = SpannableString(welcomeText)
+
+        val start = welcomeText.indexOf("GreenSpot")
+        val end = start + "GreenSpot".length
+        spannableString.setSpan(ForegroundColorSpan(Color.parseColor("#37B089")), start, end, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+        binding.welcomeText.text = spannableString
+
+        binding.signUpButton.setOnClickListener {
+
+        }
+
+        binding.signInButton.setOnClickListener {
+
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }

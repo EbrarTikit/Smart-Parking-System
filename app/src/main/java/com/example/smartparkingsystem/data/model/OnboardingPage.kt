d037b3521344449518672021328545fa59b5e857
Ebrar Tikit <20200808066@ogr.akdeniz.edu.tr>
2025-02-25 17:13:28 +0300

    Change folder structure

--- app/src/main/java/com/example/smartparkingsystem/data/model/OnboardingPage.kt
+++
@@ -1,9 +0,0 @@
-package com.example.smartparkingsystem.data.model
-
-import androidx.annotation.RawRes
-
-data class OnboardingPage(
-    @RawRes val animation: Int,
-    val title: String,
-    val description: String
-)

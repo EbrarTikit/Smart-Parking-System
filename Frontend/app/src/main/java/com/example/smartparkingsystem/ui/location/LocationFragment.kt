b04c7dbf1caf868d7b8c1b0d1aed5267be5dbca4
Ebrar Tikit <20200808066@ogr.akdeniz.edu.tr>
2025-02-26 18:23:20 +0300

    feat(location-page): control of searching

--- Frontend/app/src/main/java/com/example/smartparkingsystem/ui/location/LocationFragment.kt
+++ Frontend/app/src/main/java/com/example/smartparkingsystem/ui/location/LocationFragment.kt
@@ -1,27 +1,56 @@
 package com.example.smartparkingsystem.ui.location
 
 import android.os.Bundle
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.addTextChangedListener
 import com.example.smartparkingsystem.R
+import com.example.smartparkingsystem.databinding.FragmentLocationBinding
 
 
-class LocationFragment : Fragment() {
+class LocationFragment : Fragment(R.layout.fragment_location) {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
+    private var _binding: FragmentLocationBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_location, container, false)
+    ): View {
+        _binding = FragmentLocationBinding.inflate(inflater, container, false)
+        setupClickListeners()
+        return binding.root
     }
 
+    private fun setupClickListeners() {
+        binding.etSearch.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // Do nothing
+            }
 
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                val query = s.toString()
+                search(query)
+            }
+
+            override fun afterTextChanged(s: android.text.Editable?) {
+                // Do nothing
+            }
+        })
+
+        binding.ibBack.setOnClickListener {
+            requireActivity().onBackPressedDispatcher.onBackPressed()
+        }
+    }
+
+    private fun search(query: String) {
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }

052e0f22b4d0bb6091c2a37ad331a1b9de335aed
Ebrar Tikit <20200808066@ogr.akdeniz.edu.tr>
2025-03-14 15:24:57 +0300

    feat(user): Introduce User Service and Sign-Up Feature
    -   Added `UserService` for user-related network operations.
    -   Added `UserRepository` for handling user data and API requests.
    -   Created `SignUpRequest` and `SignUpResponse` data classes.
    -   Created `SignUpViewModel` to manage sign-up logic and validation.
    -   Created `SignUpFragment` for user sign-up UI.
    -   Created `UiState` for handling UI states.
    -   Created `Constants` for storing constant values.
    -   Updated `NetworkModule` to include Retrofit setup for `UserService`.
    -   Updated `ChatbotService` to move constants to the `Constants` class.
    -   Modified `fragment_sign_up.xml` for user interface elements.

--- Frontend/app/src/main/java/com/example/smartparkingsystem/utils/Constants.kt
+++ Frontend/app/src/main/java/com/example/smartparkingsystem/utils/Constants.kt
@@ -1,4 +1,8 @@
 package com.example.smartparkingsystem.utils
 
-class Constants {
+object Constants {
+    const val CHAT = "/api/v1/chat"
+    const val HISTORY = "/api/v1/chat/{session_id}/history"
+    const val SIGNUP = "/api/auth/signup"
+    const val SIGNIN = "/api/auth/signin"
 }

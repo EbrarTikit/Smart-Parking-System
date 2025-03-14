32e01f245b0c9503a0fd12993bd24847d7fab522
Serhat Zorlu <94406035+SerhatZorlu@users.noreply.github.com>
2025-03-15 00:34:11 +0300

    Some files renamed and swagger feature added

--- Backend/navigation-service/src/main/java/com/example/navigation_service/controller/INavigationController.java
+++ Backend/navigation-service/src/main/java/com/example/navigation_service/controller/INavigationController.java
@@ -1,13 +1,13 @@
-package com.example.navigation_service.service;
+package com.example.navigation_service.controller;
 
 import java.util.List;
 
 import com.example.navigation_service.dto.DtoCarPark;
 
-public interface ICarParkService {
-    
+public interface INavigationController {
+
     public DtoCarPark getParkLocation(Long id);
 
     public List<DtoCarPark> getAllParkLocation();
-   
+
 }
--- Backend/navigation-service/src/main/java/com/example/navigation_service/service/INavigationService.java
+++ Backend/navigation-service/src/main/java/com/example/navigation_service/service/INavigationService.java
@@ -1,13 +1,13 @@
-package com.example.navigation_service.controller;
+package com.example.navigation_service.service;
 
 import java.util.List;
 
 import com.example.navigation_service.dto.DtoCarPark;
 
-public interface ICarParkController {
-
+public interface INavigationService {
+    
     public DtoCarPark getParkLocation(Long id);
 
     public List<DtoCarPark> getAllParkLocation();
-
+   
 }

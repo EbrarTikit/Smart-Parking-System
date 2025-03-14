7dbfe6c57aed591684b966ceb1bf7fb9c5729aa7
Mervegül Çetin <mervecetin0908@gmail.com>
2025-03-14 21:47:05 +0300

    swagger implementation

--- Backend/user-service/src/main/java/com/example/user_service/dto/JwtResponse.java
+++ Backend/user-service/src/main/java/com/example/user_service/dto/JwtResponse.java
@@ -3,9 +3,11 @@ package com.example.user_service.dto;
 public class JwtResponse {
     private String token;
     private String type = "Bearer";
+    private Long userId; 
 
-    public JwtResponse(String token) {
+    public JwtResponse(String token, Long userId) {
         this.token = token;
+        this.userId = userId;
     }
 
     public String getToken() {
@@ -22,5 +24,13 @@ public class JwtResponse {
 
     public void setType(String type) {
         this.type = type;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
     }
 }
--- Backend/user-service/src/main/java/com/example/user_service/service/UserService.java
+++ Backend/user-service/src/main/java/com/example/user_service/service/UserService.java
@@ -27,6 +27,10 @@ public class UserService {
         return userRepository.findById(id);
     }
 
+    public Optional<User> findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+
     public User saveUser(User user) {
         return userRepository.save(user);
     }

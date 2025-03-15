d106e97e8a77ea3c288c42f74f5a9bdc406b474e
Serhat Zorlu <94406035+SerhatZorlu@users.noreply.github.com>
2025-03-15 03:22:57 +0300

    Docker file created

--- Backend/navigation-service/src/test/java/com/example/navigation_service/starter/NavigationServiceApplicationTests.java
+++ Backend/navigation-service/src/test/java/com/example/navigation_service/starter/NavigationServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.navigation_service;
+package com.example.navigation_service.starter;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class NavigationServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
-}
+} 

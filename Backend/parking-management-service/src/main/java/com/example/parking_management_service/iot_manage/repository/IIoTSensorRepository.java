bfbba397fd88bbd4c87bc1065fdd8d2bece82b30
Serhat Zorlu <94406035+SerhatZorlu@users.noreply.github.com>
2025-04-24 21:23:08 +0300

    iot manage subservice started

---
+++ Backend/parking-management-service/src/main/java/com/example/parking_management_service/iot_manage/repository/IIoTSensorRepository.java
@@ -0,0 +1,11 @@
+package com.example.parking_management_service.iot_manage.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.parking_management_service.iot_manage.model.IoTSensor;
+
+@Repository
+public interface IIoTSensorRepository extends JpaRepository<IoTSensor,Long>{
+
+}

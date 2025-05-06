b250bfd9f2306a7a22234c6a551fa8bd2c9f401b
Serhat Zorlu <94406035+SerhatZorlu@users.noreply.github.com>
2025-05-06 13:54:17 +0300

    Merge origin/master into iotmanage - conflict resolved

--- Backend/parking-management-service/src/main/java/com/example/parking_management_service/iot_manage/repository/IIoTSensorRepository.java
+++ Backend/parking-management-service/src/main/java/com/example/parking_management_service/iot_manage/repository/IIoTSensorRepository.java
@@ -3,18 +3,6 @@ package com.example.parking_management_service.iot_manage.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.example.parking_management_service.iot_manage.model.Sensor;
-
-@Repository
-public interface IIoTSensorRepository extends JpaRepository<Sensor,Long>{
-
-}
-
-package com.example.parking_management_service.iot_manage.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import com.example.parking_management_service.iot_manage.model.IoTSensor;
 
 @Repository

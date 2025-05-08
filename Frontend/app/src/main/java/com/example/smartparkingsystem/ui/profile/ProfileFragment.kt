50ce708e3d45191f7657331c9942c3174ddae2e1
Ebrar Tikit <20200808066@ogr.akdeniz.edu.tr>
2025-05-08 11:06:04 +0300

    feat(auth): Implement location permission check and logout
    - Added logic to `SignInFragment` to check for location permission after successful sign-in and navigate directly to the Home screen if granted, otherwise navigate to the Location Access fragment.
    - Updated `LocationAccessFragment` to log permission status and check for already granted permission before requesting.
    - Implemented logout functionality in `ProfileFragment` using `SessionManager` to clear the session and navigate back to the `SignInFragment`, clearing the navigation stack.
    - Added navigation actions in `nav_graph.xml` for `SignInFragment` to navigate to `navigation_home` and for `navigation_profile` to navigate to `signInFragment`.

--- Frontend/app/src/main/java/com/example/smartparkingsystem/ui/profile/ProfileFragment.kt
+++ Frontend/app/src/main/java/com/example/smartparkingsystem/ui/profile/ProfileFragment.kt
@@ -5,22 +5,54 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavOptions
+import androidx.navigation.fragment.findNavController
 import com.example.smartparkingsystem.R
+import com.example.smartparkingsystem.databinding.FragmentProfileBinding
+import com.example.smartparkingsystem.utils.SessionManager
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class ProfileFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    private var _binding: FragmentProfileBinding? = null
+    private val binding get() = _binding!!
 
-    }
+    @Inject
+    lateinit var sessionManager: SessionManager
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+    ): View {
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setOnClickListeners()
+
+    }
+
+    private fun setOnClickListeners() {
+        binding.btnLogout.setOnClickListener {
+            sessionManager.clearSession()
+            findNavController().navigate(
+                R.id.action_navigation_profile_to_signInFragment,
+                null,
+                NavOptions.Builder()
+                    .setPopUpTo(R.id.nav_graph, true) // navigation graph'ın en üstüne kadar temizle
+                    .build()
+            )
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 
 }

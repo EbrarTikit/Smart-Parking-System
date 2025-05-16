618f2f1980cf2a0bf3f7d0981f6ba42715892c54
Ebrar Tikit <20200808066@ogr.akdeniz.edu.tr>
2025-05-16 14:26:38 +0300

    feat(notification): Implement notification permission and preferences
    - Added `POST_NOTIFICATIONS` permission to `AndroidManifest.xml`.
    - Implemented notification permission request flow in `HomeFragment` for Android 13 and above.
    - Added `NotificationPreferences` data class.
    - Added `getNotificationPreferences` and `toggleNotificationPreferences` to `UserService` and `UserRepository`.
    - Added `updateNotificationPreferences` to `HomeViewModel` to update user's notification preferences.
    - Defined `NOTIFICATION_PREFERENCE` and `NOTIFICATION_TOGGLE` constants for API endpoints.
    - Injected `SessionManager` into `HomeFragment` to retrieve user ID.

--- Frontend/app/src/main/java/com/example/smartparkingsystem/data/remote/UserService.kt
+++ Frontend/app/src/main/java/com/example/smartparkingsystem/data/remote/UserService.kt
@@ -2,6 +2,7 @@ package com.example.smartparkingsystem.data.remote
 
 import com.example.smartparkingsystem.data.model.FavoriteListResponse
 import com.example.smartparkingsystem.data.model.FavoriteResponse
+import com.example.smartparkingsystem.data.model.NotificationPreferences
 import com.example.smartparkingsystem.data.model.SignInRequest
 import com.example.smartparkingsystem.data.model.SignInResponse
 import com.example.smartparkingsystem.data.model.SignUpRequest
@@ -9,6 +10,8 @@ import com.example.smartparkingsystem.data.model.SignUpResponse
 import com.example.smartparkingsystem.utils.Constants.ADD_FAVORITE
 import com.example.smartparkingsystem.utils.Constants.DELETE_FAVORITE
 import com.example.smartparkingsystem.utils.Constants.GET_FAVORITE
+import com.example.smartparkingsystem.utils.Constants.NOTIFICATION_PREFERENCE
+import com.example.smartparkingsystem.utils.Constants.NOTIFICATION_TOGGLE
 import com.example.smartparkingsystem.utils.Constants.SIGNIN
 import com.example.smartparkingsystem.utils.Constants.SIGNUP
 import retrofit2.Response
@@ -16,6 +19,7 @@ import retrofit2.http.Body
 import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.PUT
 import retrofit2.http.Path
 
 interface UserService {
@@ -46,5 +50,15 @@ interface UserService {
         @Path("userId") userId: Int,
         @Path("parkingId") parkingId: Int
     ): Response<Unit>
+
+    @GET(NOTIFICATION_PREFERENCE)
+    suspend fun getNotificationPreferences(
+        @Path("userId") userId: Int
+    ): Response<NotificationPreferences>
+
+    @PUT(NOTIFICATION_TOGGLE)
+    suspend fun toggleNotificationPreferences(
+        @Path("userId") userId: Int
+    ): Response<NotificationPreferences>
 
 }

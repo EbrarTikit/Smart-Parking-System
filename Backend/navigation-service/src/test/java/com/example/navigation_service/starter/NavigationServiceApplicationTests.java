11fdccac04aed828499e3e56e5f655a8aee0af0e
Serhat Zorlu <94406035+SerhatZorlu@users.noreply.github.com>
2025-05-27 23:51:22 +0300

    some changes made

--- Backend/navigation-service/src/test/java/com/example/navigation_service/starter/NavigationServiceApplicationTests.java
+++ Backend/navigation-service/src/test/java/com/example/navigation_service/starter/NavigationServiceApplicationTests.java
@@ -2,12 +2,11 @@ package com.example.navigation_service.starter;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class NavigationServiceApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
 
 } 
